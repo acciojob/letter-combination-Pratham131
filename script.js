fa580b04a38b38d59a0f943201b68ec80bf8f600
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-06-16 09:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
+	let table = {
+		"2":"abc",
+		"3":"def",
+		"4":"ghi",
+		"5":"jkl",
+		"6":"mno",
+		"7":"pqrs",
+		"8":"tuv",
+		"9":"xyz"
+	}
+	let solutions = [];
+	
+	function solve(currentIndex, calculatedString) {
+		if(input.length <= currentIndex){
+			solutions.push(calculatedString);
+			return;
+		}
+		// currentIndex = 2, calculatedString = "dm"
+		let currentChar = input[currentIndex] //3
+		// table["3"] = "def"
+		for (let i = 0; i < table[currentChar].length; i++) {
+			//def
+			solve(currentIndex+1, calculatedString+table[calculatedString])
+			// solve (0+1m "" + "d")
+		}
+	}
+
+	solve(0, "");
+	return solutions;
 }
 
 module.exports = letterCombinations;
